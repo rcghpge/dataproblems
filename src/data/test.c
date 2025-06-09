83b530aa229806b0a7a47f911e72e0a6fa8e1a33
Robert Cocker <rcocker87@gmail.com>
2025-06-09 17:41:46 -0500

    setting up directory for 4380

---
+++ src/data/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("data directory placemarker.\n");
+    return 0;
+}
---
+++ src/homework/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("homework placemarker\n");
+    return 0;
+}
